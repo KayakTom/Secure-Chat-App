b471fa6ef14c4967359c8846075cfc304d887491
KayakTom <tom159cnc65@outlook.com>
2025-02-12 09:20:18 +0000

    Basic GUI

--- secure-chat-app/src/App.js
+++ secure-chat-app/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import './styles.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function SendButton(){
+    return(
+        <button name = "sendMessage" className = "message">SEND</button>
+    );
 }
 
-export default App;
+function ChatBox(){
+    return(
+        <input name = "userMessage" type = "text" className = "message" placeholder = "Enter Message Here..."></input>
+    );
+}
+
+function ChatMessage(){
+    return(
+        <p></p>
+    );
+
+}
+
+function chatInterface(){
+    return(
+    <>
+        <div className = "title">
+            <h1>Secure Chat App</h1>
+            <div className = "creator">
+                <h2>By: Tom Cowan</h2>
+            </div>
+        </div>
+        <div className = "message">
+            <ChatBox />
+                <SendButton />
+        </div>
+
+        <div className = "chat">
+            <ChatMessage />
+        </div>
+    </>
+    );
+}
+
+export default chatInterface;
